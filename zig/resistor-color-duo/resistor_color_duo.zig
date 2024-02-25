078ae36511a52853981a8f1317fd30082b4f9e9e
FabPch <fabien191@hotmail.com>
2024-02-25 19:38:46 +0100

    resistor duo

---
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -0,0 +1,5 @@
+pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+
+pub fn colorCode(colors: [2]ColorBand) usize {
+    return @as(usize, @intFromEnum(colors[0])) * 10 + @as(usize, @intFromEnum(colors[1]));
+}
